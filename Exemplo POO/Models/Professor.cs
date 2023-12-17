b09937f127004611d4ec8dcb78b075a362a12883
Edigar Maia <edigarmaia@gmail.com>
2023-12-17 14:50:30 -0300

    Construtor por heranca

--- Exemplo POO/Models/Professor.cs
+++ Exemplo POO/Models/Professor.cs
@@ -7,6 +7,10 @@ namespace Exemplo_POO.Models
 {
     public class Professor : Pessoa
     {
+        public Professor(string nome) : base(nome)
+        {
+            
+        }
         public decimal Salario { get; set; }
 
         public override void Apresentar()
--- Exemplo POO/Program.cs
+++ Exemplo POO/Program.cs
@@ -6,27 +6,36 @@ p1.Idade = 41;
 
 p1.Apresentar();
 */
-
+/*
 ContaCorrente c1 = new ContaCorrente(123, 1000); 
 c1.ExibirSaldo();
 c1.Sacar(500);
 c1.ExibirSaldo();
 
 Console.WriteLine();
-
+*/
+/*
 Aluno a1 = new Aluno();
 a1.Nome = "Edigar";
 a1.Idade = 40;
 a1.Nota = 10;
 a1.Apresentar();
+*/
 
-
+/*
 Professor p1 = new Professor();
 p1.Nome = "Edigar";
 p1.Idade = 40;
 p1.Salario = 1000;
 p1.Apresentar();
+*/
 
+/*
 Corrente c = new Corrente();
 c.Creditar(500);
 c.ExibirSaldo(); 
+*/
+
+Pessoa p1 = new Pessoa("Edigar");   
+Aluno a1 = new Aluno("Maria");
+a1.Apresentar();
