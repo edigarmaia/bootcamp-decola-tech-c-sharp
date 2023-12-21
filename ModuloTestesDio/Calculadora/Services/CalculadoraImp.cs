33dece71c6164408ef250fac06e88c7d8805f71c
Edigar Maia <edigarmaia@gmail.com>
2023-12-21 10:45:17 -0300

    Mais testes das operações matemáticas

--- ModuloTestesDio/Calculadora/Services/CalculadoraImp.cs
+++ ModuloTestesDio/Calculadora/Services/CalculadoraImp.cs
@@ -11,5 +11,19 @@ namespace Calculadora.Services
         {
             return num1 + num2;
         }
+
+        public int Subtrair(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Dividir(int num1, int num2)
+        {
+            return num1 / num2;
+        }
     }
 }
--- ModuloTestesDio/CalculadoraTestes/CalculadoraTests.cs
+++ ModuloTestesDio/CalculadoraTestes/CalculadoraTests.cs
@@ -23,4 +23,60 @@ public class CalculadoraTests
         Assert.Equal(15, resultado);
 
     }
+
+     [Fact]
+    public void DeveSomar10Com10ERetornar20()
+    {
+        // Arrange - montar cenário
+        int num1 = 10;
+        int num2 = 10;
+
+        // Act - chamar o cenário
+        int resultado = _calc.Somar(num1, num2);
+
+        // Assert - validar se teve resultado esperado
+        Assert.Equal(20, resultado);
+
+    }
+    [Fact]
+    public void DeveSubtrair20Menos10ERetornar10()
+    {
+        // Arrange
+        int num1 = 20;
+        int num2 = 10;
+
+        // Act
+        int resultado = _calc.Subtrair(num1, num2);
+
+        // Assert
+        Assert.Equal(10, resultado);
+    }
+
+     [Fact]
+    public void DeveMultiplicar10Com15ERetornar150()
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 15;
+
+        // Act
+        int resultado = _calc.Multiplicar(num1, num2);
+
+        // Assert
+        Assert.Equal(150, resultado);
+    }
+
+     [Fact]
+    public void DeveDividir100Por10ERetornar10()
+    {
+        // Arrange
+        int num1 = 100;
+        int num2 = 10;
+
+        // Act
+        int resultado = _calc.Dividir(num1, num2);
+
+        // Assert
+        Assert.Equal(10, resultado);
+    }
 }
