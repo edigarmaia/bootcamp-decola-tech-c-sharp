2c71a94002004a63f07b38f2b77276f445808dbd
Edigar Maia <edigarmaia@gmail.com>
2023-12-21 10:22:17 -0300

    Criado classe de testes

---
+++ ModuloTestesDio/CalculadoraTestes/CalculadoraTests.cs
@@ -0,0 +1,20 @@
+using Calculadora.Services;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTests
+{
+    private CalculadoraImp _calc;
+    public CalculadoraTests()
+    {
+        _calc = new CalculadoraImp();
+    }
+    [Fact]
+    public void DeveSomar5Com10ERetornar15()
+    {
+        // Arrange - montar cenário
+        // Act - chamar o cenário
+        // Assert - validar se teve resultado esperado
+
+    }
+}
