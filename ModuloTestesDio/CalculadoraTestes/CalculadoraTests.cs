fbda9d89f40c8fecab156a03aff082ff83f38a61
Edigar Maia <edigarmaia@gmail.com>
2023-12-21 10:30:01 -0300

    Implementando o teste unitário

--- ModuloTestesDio/CalculadoraTestes/CalculadoraTests.cs
+++ ModuloTestesDio/CalculadoraTestes/CalculadoraTests.cs
@@ -13,8 +13,14 @@ public class CalculadoraTests
     public void DeveSomar5Com10ERetornar15()
     {
         // Arrange - montar cenário
+        int num1 = 5;
+        int num2 = 10;
+
         // Act - chamar o cenário
+        int resultado = _calc.Somar(num1, num2);
+
         // Assert - validar se teve resultado esperado
+        Assert.Equal(15, resultado);
 
     }
 }
