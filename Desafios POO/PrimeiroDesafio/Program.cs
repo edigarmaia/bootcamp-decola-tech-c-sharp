eb80c0789b7b28b98acc3632dc6fac679bd4ab12
Edigar Maia <edigarmaia@gmail.com>
2023-12-22 13:27:43 -0300

    Primeiro desafio de codigo POO

---
+++ Desafios POO/PrimeiroDesafio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Pessoa
+{
+    private string nome;
+    private int idade;
+
+    public Pessoa(string n, int i)
+    {
+        nome = n;
+        idade = i;
+    }
+
+    public string GetNome()
+    {
+        return nome;
+    }
+
+    public int GetIdade()
+    {
+        return idade;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Informe o nome da pessoa: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Informe a idade da pessoa: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        Pessoa pessoa = new Pessoa(nome, idade);
+
+        Console.WriteLine($"Nome: {pessoa.GetNome()}, Idade: {pessoa.GetIdade()}");
+    }
+}
