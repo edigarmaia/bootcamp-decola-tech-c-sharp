9ea508a8662dfb5b2ffb0b8f40155d14740ffa90
Edigar Maia <edigarmaia@gmail.com>
2023-12-23 02:26:49 -0300

    Desafio heranca RPG

---
+++ Desafios POO/DesafioHeranca/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Personagem
+{
+    public string Nome { get; set; }
+    public int Mana { get; set; }
+
+    public Personagem(string nome, int mana)
+    {
+        Nome = nome;
+        Mana = mana;
+    }
+}
+
+class Subclasse : Personagem
+{
+    public int DanoBase { get; set; }
+
+    public Subclasse(string nome, int mana, int danoBase) : base(nome, mana)
+    {
+        DanoBase = danoBase;
+    }
+
+    public void CalcularDano()
+    {
+        Console.WriteLine(Nome + " atacou e causou " + DanoBase * Mana + " de dano!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string nome = Console.ReadLine();
+        int mana = Convert.ToInt32(Console.ReadLine());
+        int danoBase = Convert.ToInt32(Console.ReadLine());
+
+        Subclasse sb = new Subclasse(nome, mana, danoBase);
+
+        sb.CalcularDano();
+       
+    }
+}
